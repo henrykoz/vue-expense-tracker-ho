d536b2485d145b2529664e24f467e7a48aacfe03
henrykoz <h3nryk.ozog@gmail.com>
2023-12-17 12:09:49 +0000

    balance logic

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <Header />
   <div class="container">
-    <Balance />
+    <Balance :total="total" />
     <IncomeExpense />
-    <TransactionList />
+    <TransactionList :transactions="transactions" /> // passing a prop
     <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpense from "./components/IcomeExpense.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpense,
-    TransactionList,
-    AddTransaction,
-  },
-};
+import { ref, computed } from "vue";
+
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+]);
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
 </script>
